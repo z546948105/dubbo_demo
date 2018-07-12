bf5a5c36d372bd0ecb4f96593a8bbffee886eb47
zhongwenchao <738327089@qq.com>
2018-07-12 14:35:04 +0800

    1

--- hjz-dubbo-provider/src/main/java/com/hjz/service/DubboServiceProvider.java
+++ hjz-dubbo-provider/src/main/java/com/hjz/service/DubboServiceProvider.java
@@ -7,7 +7,6 @@ import com.hjz.dubbo.api.DubboServiceTest;
 @Service("dubboServiceTest")
 public class DubboServiceProvider implements DubboServiceTest {
 
-	@Override
 	public int calculate(int a, int b) {
 		return a+b;
 	}
